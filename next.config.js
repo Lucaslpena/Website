29e22517cb8af957d937fdbab861f37fd76e9ad5
Lucas Lorenzo Pena <lucaslpena@gmail.com>
2024-08-30 17:09:59 -0400

    forwarded to subscription page

--- next.config.js
+++ next.config.js
@@ -19,6 +19,11 @@ module.exports = {
                 destination: 'https://lucaslorenzop.notion.site/lucaslorenzop/Writing-Index-5ddfe015112e4ba286dedfe5d9fb24d0/',
                 permanent: true,
             },
+            {
+                source: '/subscription',
+                destination: 'https://lucaslorenzop.notion.site/Creative-Tech-Subscription-fccd83a1a87440f1a123d6ab065422fc?pvs=4',
+                permanent: true,
+            },
         ]
     },
 }
